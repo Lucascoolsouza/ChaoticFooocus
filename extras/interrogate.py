114031860c67ac25ad52faf49984522f0600016b
Lucascoolsouza <lucascoolsouza2@gmail.com>
2025-07-22 17:10:51 -0300

    Update interrogate.py

--- extras/interrogate.py
+++ extras/interrogate.py
@@ -29,7 +29,7 @@ class Interrogator:
         if self.blip_model is None:
             # Load BLIP-2 model and processor from Hugging Face
             model_name = "Salesforce/blip2-opt-2.7b" # Using a specific BLIP-2 model
-            self.processor = AutoProcessor.from_pretrained(model_name, local_files_only=False)
+            self.processor = Blip2Processor.from_pretrained(model_name, local_files_only=False)
             model = Blip2ForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16, local_files_only=False)
 
             model.eval()
