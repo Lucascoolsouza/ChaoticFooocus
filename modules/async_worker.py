7b1ffd84706b1e9f95189932a557c027a1f9fbdc
Lucascoolsouza <lucascoolsouza2@gmail.com>
2025-07-23 02:45:31 -0300

    Revert "Update async_worker.py"
    This reverts commit b3ee11fa0eaf4145e456eed50e3b617692dcadfd.

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -60,14 +60,7 @@ class AsyncTask:
                                         range(default_max_lora_number)])
         self.input_image_checkbox = args.pop()
         self.current_tab = args.pop()
-        uov_method_value = args.pop() if args else None
-        # Defensive: ensure uov_method is always a string
-        if isinstance(uov_method_value, bool):
-            self.uov_method = str(uov_method_value)
-        elif uov_method_value is None:
-            self.uov_method = ""
-        else:
-            self.uov_method = uov_method_value
+        self.uov_method = args.pop()
         self.uov_input_image = args.pop()
         self.outpaint_selections = args.pop()
         self.inpaint_input_image = args.pop()
@@ -146,13 +139,7 @@ class AsyncTask:
 
         self.enhance_input_image = args.pop()
         self.enhance_checkbox = args.pop()
-        enhance_uov_method_value = args.pop() if args else None
-        if isinstance(enhance_uov_method_value, bool):
-            self.enhance_uov_method = str(enhance_uov_method_value)
-        elif enhance_uov_method_value is None:
-            self.enhance_uov_method = ""
-        else:
-            self.enhance_uov_method = enhance_uov_method_value
+        self.enhance_uov_method = args.pop()
         self.enhance_bg_removal_model = args.pop()
         import modules.image_effects
 
