7361cfcb1b0a8687a758cdb2c29210ebf4f5f3ad
Lucascoolsouza <lucascoolsouza2@gmail.com>
2025-07-23 00:56:05 -0300

    Update patch_precision.py

--- modules/patch_precision.py
+++ modules/patch_precision.py
@@ -20,7 +20,7 @@ def patched_timestep_embedding(timesteps, dim, max_period=10000, repeat_only=Fal
         freqs = torch.exp(
             -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
         ).to(device=timesteps.device)
-        args = timesteps[:, None].float() * freqs[None]
+        args = timesteps.reshape(-1, 1).float() * freqs[None]
         embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
         if dim % 2:
             embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
