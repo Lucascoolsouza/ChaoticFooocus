7604b90f0b87774487c6d5b1ecd8e9a1169570af
Lucascoolsouza <lucascoolsouza2@gmail.com>
2025-07-26 03:33:47 -0300

    Update webui.py

--- webui.py
+++ webui.py
@@ -1155,6 +1155,8 @@ with shared.gradio_root:
             
         if not args_manager.args.disable_metadata:
             ctrls += [save_metadata_to_images, metadata_scheme]
+        else:
+            ctrls += [gr.State(False), gr.State(modules.flags.MetadataScheme.FOOOCUS.value)]
             
         ctrls += [detail_daemon_enabled, detail_daemon_amount, detail_daemon_start, detail_daemon_end, 
                   detail_daemon_bias, detail_daemon_base_multiplier, detail_daemon_start_offset, detail_daemon_end_offset, 
